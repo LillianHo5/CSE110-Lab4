b7e4bf3e191c7e6415621cd2f29be775263bbcd7
LillianHo5 <92070679+LillianHo5@users.noreply.github.com>
2023-01-31 14:19:04 -0800

    Exercise #1 Implementation

--- app/src/main/java/edu/ucsd/cse110/lab4/ProfileActivity.java
+++ app/src/main/java/edu/ucsd/cse110/lab4/ProfileActivity.java
@@ -6,16 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void onLaunchProfileClicked(View view) {
-        Intent intent = new Intent(this, ProfileActivity.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_profile);
     }
 }
